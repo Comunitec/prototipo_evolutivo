929bf3c3e08e9344e15c3787e598d6c643e868de
Mariane Limoli <marianelimoli@gmail.com>
2023-10-25 14:46:05 -0300

    componentes do formulario de cadastro de curso

--- FrontEnd/src/app/components/course-form/course-form.component.ts
+++ FrontEnd/src/app/components/course-form/course-form.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
-  styleUrls: ['./course-form.component.css']
+  styleUrls: ['./course-form.component.css'],
 })
 export class CourseFormComponent {
+  tagListOpen = false;
+  postContent = '';
+  tagList = ['Angular', 'JavaScript', 'TypeScript', 'HTML', 'CSS'];
+  selectedTag: string | undefined;
 
+  openTagSelector() {
+    this.tagListOpen = true;
+  }
+
+  savePost() {
+    // Aqui você pode implementar a lógica para salvar o conteúdo do post, por exemplo, em um serviço ou no banco de dados.
+    console.log('Conteúdo salvo:', this.postContent);
+
+    // Feche o editor após salvar.
+    this.tagListOpen = false;
+    this.postContent = '';
+  }
 }
