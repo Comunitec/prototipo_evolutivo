44ce6713643ec973247bc3f408932bee7252eb9e
willianComunitec <williansoaresfer@hotmail.com>
2023-11-18 14:05:26 -0300

    atualização do xp, criado managaCourses

---
+++ FrontEnd/src/app/pages/manage-course/manage-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-course',
+  templateUrl: './manage-course.component.html',
+  styleUrls: ['./manage-course.component.css']
+})
+export class ManageCourseComponent {
+
+}
