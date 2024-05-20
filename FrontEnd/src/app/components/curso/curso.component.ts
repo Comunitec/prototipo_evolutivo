a7057266a82e96c6c051e272d3de40e6e93cd463
Mariane Limoli <marianelimoli@gmail.com>
2024-05-20 20:19:20 -0300

    ajustes no front

--- FrontEnd/src/app/components/curso/curso.component.ts
+++ FrontEnd/src/app/components/curso/curso.component.ts
@@ -6,7 +6,7 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './curso.component.html',
   styleUrls: ['./curso.component.css']
 })
-export class CursoComponent implements OnInit {  
+export class CursoComponent implements OnInit {
   nomeCurso = "Typescript";
   descricao = "O curso de TypeScript oferece uma introdução abrangente a essa linguagem de programação superset do JavaScript, abordando desde conceitos básicos até tópicos avançados. Os alunos aprenderão a criar aplicativos robustos e escaláveis, aproveitando os recursos de tipagem estática, interfaces e classes. Com ênfase na segurança e manutenibilidade do código, o curso explora como TypeScript facilita o desenvolvimento de projetos complexos, tornando-os mais legíveis e menos propensos a erros. Ao final, os participantes estarão aptos a utilizar TypeScript eficientemente em diversas aplicações, aumentando sua produtividade como desenvolvedores.";
   imagem = "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*moJeTvW97yShLB7URRj5Kg.png";
@@ -38,5 +38,13 @@ export class CursoComponent implements OnInit {
   redirecionarParaRota(idCurso: string) {
     // Aqui você pode redirecionar para a rota desejada, passando o id do curso
     this.router.navigate(['/sua-rota', idCurso]);
+  }
+
+  getTruncatedText(text: string, limit: number): string {
+    if (text.length > limit) {
+      return text.substring(0, limit) + '...';
+    } else {
+      return text;
+    }
   }
 }
