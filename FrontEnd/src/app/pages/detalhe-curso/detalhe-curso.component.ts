3b6e490bd03dcf6f9d1923996741b119ee8a1128
Mariane Limoli <marianelimoli@gmail.com>
2024-05-29 20:21:20 -0300

    ajustes

--- FrontEnd/src/app/pages/detalhe-curso/detalhe-curso.component.ts
+++ FrontEnd/src/app/pages/detalhe-curso/detalhe-curso.component.ts
@@ -10,6 +10,7 @@ import { ModalMatRealizadaComponent } from 'src/app/components/modal-mat-realiza
 export class DetalheCursoComponent {
   nomeCurso = "Typescript";
   finishTotal = "35";
+  dataAprovacao = "10/10/2021";
   nomeCriador = "Eduardo Abreu";
   descricao = "O curso de TypeScript oferece uma  \nintrodução abrangente a essa linguagem de programação superset do JavaScript, \nabordando desde conceitos básicos até tópicos avançados. Os alunos aprenderão a criar aplicativos robustos e escaláveis, aproveitando os recursos de tipagem estática, interfaces e classes. Com ênfase na segurança e manutenibilidade do código, o curso explora como TypeScript facilita o desenvolvimento de projetos complexos, tornando-os mais legíveis e menos propensos a erros. Ao final, os participantes estarão aptos a utilizar TypeScript eficientemente em diversas aplicações, aumentando sua produtividade como desenvolvedores.";
   imagem = "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*moJeTvW97yShLB7URRj5Kg.png";
@@ -25,7 +26,7 @@ export class DetalheCursoComponent {
   openModal(): void {
     const dialogRef = this.dialog.open(ModalMatRealizadaComponent, {
       width: '350px'
-      
+
     });
     this.matricula = false;
     this.verAulas = true;
